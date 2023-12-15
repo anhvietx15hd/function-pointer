82b8464ec3912fb9afc3b882dc75334a0f36c0e3
anhvietx15hd <anhvietx15hd@gmail.com>
2023-12-15 14:15:30 +0700

    fix bug

--- load_function_dynamically_device_management/main.c
+++ load_function_dynamically_device_management/main.c
@@ -6,13 +6,38 @@
 #include "Device_Manage.h"
 #include "IO.h"
 /******************************************************************************
+ * TYPEDEFS
+******************************************************************************/
+typedef enum {
+    ADD             = 0,
+    DELETE          = 1,
+    SHOW            = 2,
+    NO_COMMAND      = 3
+} Device_Command_t;
+/*Define the function pointer prototype*/
+typedef Device_Manage_Error_Code_t (*device_command)(void);
+
+/******************************************************************************
+ *PROTOTYPES
+******************************************************************************/
+Device_Manage_Error_Code_t Add(void);
+Device_Manage_Error_Code_t Delete(void);
+Device_Manage_Error_Code_t Show(void);
+/******************************************************************************
+ * VARIABLES
+******************************************************************************/
+device_command command[] = {Add, Delete, Show};
+/******************************************************************************
  * DEFINITIONS
 ******************************************************************************/
 void main() {
+    /*Show the address of functions*/
+    printf("%x %x %x\n", command[ADD], command[DELETE], command[SHOW]);
+    
     Device_Data_t device_information;
     Device_Manage_Error_Code_t error_code;
     uint16_t option = 0;
-    uint16_t position;
+    Device_Command_t cmd_option = NO_COMMAND;
 
     do {
         printf("\n___________________________________________________________________\n");
@@ -29,22 +54,16 @@ void main() {
         /*option handling*/
         switch (option) {
             case 'a':
-                Device_Create_Information(&device_information);
-                printf("Enter the position to add: ");
-                fflush(stdin);
-                scanf("%d", &position);
-                error_code = Device_Add(position, &device_information);
+                /*Assign the index of function pointer in function pointer array*/
+                cmd_option = ADD;
                 break;
             
             case 'd':
-                printf("Enter the position to delete: ");
-                fflush(stdin);
-                scanf("%d", &position);
-                error_code = Device_Delete(position);
+                cmd_option = DELETE;
                 break;
 
             case 's':
-                error_code = Device_Display_All();
+                cmd_option = SHOW;
                 break;
 
             case 'e':
@@ -55,9 +74,43 @@ void main() {
                 error_code = INVALID_COMMAND;
                 break;
         }
-
+        if(cmd_option != NO_COMMAND) {
+            /*Call function*/
+            error_code = command[cmd_option]();
+        }
+        /*Show the message after the action is done*/
         printf("\n%s\n", IO_Message(error_code));
     } while (option != 'e');
+}
+
+Device_Manage_Error_Code_t Add(void) {
+    Device_Manage_Error_Code_t error_code;
+    uint16_t position;
+    Device_Data_t device_information;
+
+    Device_Create_Information(&device_information);
+    printf("Enter the position to add: ");
+    fflush(stdin);
+    scanf("%d", &position);
+    error_code = Device_Add(position, &device_information);
+
+    return error_code;
+}
+
+Device_Manage_Error_Code_t Delete(void) {
+    Device_Manage_Error_Code_t error_code;
+    uint16_t position;
+
+    printf("Enter the position to delete: ");
+    fflush(stdin);
+    scanf("%d", &position);
+    error_code = Device_Delete(position);
+
+    return error_code;
+}
+
+Device_Manage_Error_Code_t Show(void) {
+    return Device_Display_All();
 }
 /******************************************************************************
  * END OF FILE
